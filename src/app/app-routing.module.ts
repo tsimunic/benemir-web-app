575acc31c9ba0fa4676515743083f7fda17f9117
Toma Šimunić <tsimunic93@gmail.com>
2021-02-05 20:19:48 +0100

    v0.0.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent},
   { path: 'shop', component: ShopComponent},
   { path: 'history', component: HistoryComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component:  HomeComponent}
 ];
 
 @NgModule({
